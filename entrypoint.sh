0438460460d8d6232ef3207faecd31f0d6cee696
William Pavei Antero <williamantero@gmail.com>
2020-10-14 11:34:27 -0300

    Adding rails webpacker install

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,8 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /myapp/tmp/pids/server.pid
 
+# Install rails webpacker
+rails webpacker:install
+
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
